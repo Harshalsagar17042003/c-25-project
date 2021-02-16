256962724f75a2449365b970a8d2fc8d2ab14d5c
Harshalsagar17042003 <76116106+Harshalsagar17042003@users.noreply.github.com>
2021-02-16 21:03:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
 	
 	groundObject=new ground(width/2,670,width,20);
 	dustbin=new dustbin(1200,650);
-    paper=new Paper(200,100);
+  paper=new Paper(200,100);
 	Engine.run(engine);
   
 }
